18dff904566ccec2f336e3c7eb70f8658fdf7953
Gleb Safonov <safonovgleb@gmail.com>
2025-11-16 14:58:40 +0000

    Add infra configs and production deploy

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,11 +3,20 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', 'dev-insecure-default-change-me')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '89.169.181.119',
+    '127.0.0.1',
+    'localhost',
+    'kitty.89.169.181.119.sslip.io',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://kitty.89.169.181.119.sslip.io',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -90,10 +99,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/kittygram/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
